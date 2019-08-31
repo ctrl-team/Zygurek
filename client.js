d6fc547dfcef59d867a0d73b48355632522909a3
Nimplex <39964594+Nimplex@users.noreply.github.com>
2019-08-31 22:46:37 +0200

    Add files via upload

--- client.js
+++ client.js
@@ -3,13 +3,13 @@ function zegareczek(){
   godzina = new Date().getHours();
   let minuta = new Date().getMinutes();
   if(godzina < 10 && godzina >= 4){
-    document.body.style.background = "url('/poranek.png')"
+    document.body.style.background = "url('./poranek.png')"
   }
   else if(godzina < 20 && godzina >= 10){
-    document.body.style.background = "url('/popoludnie.png')"
+    document.body.style.background = "url('./popoludnie.png')"
   }
   else{
-    document.body.style.background = "url('/noc.png')"
+    document.body.style.background = "url('./noc.png')"
   }
   document.getElementById("godzinka").innerHTML = godzina + ":" + minuta;
   setTimeout("zegareczek()" , 1000)
